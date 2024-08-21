38b18442ab9dd9468bdf2797365de2283b2ebc65
kirillz <kziborov@ptsecurity.com>
2024-08-22 00:46:29 +0300

    NEW: logging with zap

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/KirillZiborov/lnkshortener/cmd/shortener/config"
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
+var sugar zap.SugaredLogger
 var urlStore = make(map[string]string)
 
 func generateID() string {
@@ -56,18 +59,87 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
+func LoggingMiddleware() func(h http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			start := time.Now()
+
+			responseData := &responseData{
+				status: 0,
+				size:   0,
+			}
+
+			ww := &loggingResponseWriter{ResponseWriter: w, responseData: responseData}
+
+			h.ServeHTTP(ww, r)
+
+			duration := time.Since(start)
+
+			sugar.Infoln(
+				"uri", r.RequestURI,
+				"method", r.Method,
+				"status", responseData.status,
+				"duration", duration,
+				"size", responseData.size,
+			)
+		})
+	}
+}
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
 func main() {
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar = *logger.Sugar()
+
 	cfg := config.NewConfig()
 
 	r := chi.NewRouter()
 
+	r.Use(LoggingMiddleware())
+
 	r.Post("/", PostHandler(cfg.BaseURL))
 	r.Get("/{id}", GetHandler)
 
-	fmt.Printf("Server is running at %s\n", cfg.Address)
+	sugar.Infow(
+		"Starting server at",
+		"addr", cfg.Address,
+	)
 
-	err := http.ListenAndServe(cfg.Address, r)
+	err = http.ListenAndServe(cfg.Address, r)
 	if err != nil {
-		panic(err)
+		sugar.Fatalw(err.Error(), "event", "start server")
 	}
 }
